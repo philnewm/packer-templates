33f6070072d980c1c12d4c56af2bb89e1d913d0d
philnewm <Philippkd8@gmail.com>
2024-09-07 20:24:03 +0200

    Update test script
    Add in testing TODOs

--- packer_test.sh
+++ packer_test.sh
@@ -1,16 +1,5 @@
-distro=$1
-version=$2
-
-# Cleanup exisiting boxes
-vagrant destroy -f
-rm *.box || true
-rm -Rf output-* || true
-rm Vagrantfile || true
-rm -Rf .vagrant/
-
 # Build vbox image
-packer build -on-error=ask -only=virtualbox-iso.$distro-$version .
-vagrant box remove $distro$version
-vagrant box add --name $distro$version $distro-Vagrant-virtualbox-$version.x86_64.box
-vagrant init $distro$version
+packer build -on-error=ask -only=virtualbox-iso.ubuntu-2204 .
+vagrant box add --name ubuntu Ubuntu-2204-Vagrant-virtualbox-9.4.x86_64.box
+vagrant init ubuntu
 vagrant up
