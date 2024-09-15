7b42f85c914d38b818c44d58ac53b5c0d7245705
philnewm <Philippkd8@gmail.com>
2024-09-15 16:23:25 +0200

    Re-enable gnome role

--- packer_test.sh
+++ packer_test.sh
@@ -1,3 +1,7 @@
+rm Vagrantfile || true
+rm -R .vagrant || true
+vagrant box remove ubuntu || true
+
 # Build vbox image
 packer build -on-error=ask -only=virtualbox-iso.ubuntu-2204 .
 vagrant box add --name ubuntu Ubuntu-2204-Vagrant-virtualbox-9.4.x86_64.box
