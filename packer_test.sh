56faa41c51ef8cf1fc711c5409c49627ebe64d14
PhilNewm <Philippkd8@gmail.com>
2024-09-17 19:12:45 +0200

    Fix assigned default user for gnome role

--- packer_test.sh
+++ packer_test.sh
@@ -1,9 +1,9 @@
 rm Vagrantfile || true
 rm -R .vagrant || true
-vagrant box remove ubuntu || true
+vagrant box remove alma9 || true
 
 # Build vbox image
-packer build -on-error=ask -only=virtualbox-iso.ubuntu-2204 .
-vagrant box add --name ubuntu Ubuntu-2204-Vagrant-virtualbox-9.4.x86_64.box
-vagrant init ubuntu
+packer build -on-error=ask -only=virtualbox-iso.almalinux-9 .
+vagrant box add --name alma9 AlmaLinux-9-Vagrant-virtualbox-9.4.x86_64.box
+vagrant init alma9
 vagrant up
